f426ddec8dc950b4afd4312d93f6615793739611
aapiskotin <aapiskotin@gmail.com>
2019-03-22 04:47:50 +0300

    wordcounter testready

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager
+from multiprocessing import Pool
 import os
 
 
@@ -16,4 +16,21 @@ def word_count_inference(path_to_dir):
     :return: словарь, где ключ - имя файла, значение - число слов +
         специальный ключ "total" для суммы слов во всех файлах
     '''
-    raise NotImplementedError
+    files = list(os.listdir(path_to_dir))
+    counts = []
+    with Pool(len(files)) as proc:
+        counts = proc.map(count_words, list(map(lambda k: path_to_dir + '/' + k, files)))
+
+    result_dict = dict(zip(files, counts))
+    result_dict['total'] = sum(counts)
+
+    return result_dict
+
+
+def count_words(path_to_file):
+    with open(path_to_file, 'r') as f:
+        count = 0
+        for line in list(filter(lambda k: k != '\n', f.readlines())):
+            count += len(line.split(' '))
+
+        return count
