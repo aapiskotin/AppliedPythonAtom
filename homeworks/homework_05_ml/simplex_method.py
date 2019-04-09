affcb2b39dc57b101e948e587f7a819cdf99d524
aapiskotin <aapiskotin@gmail.com>
2019-04-09 03:33:04 +0300

    simplex testready

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,22 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+    b = b.reshape(-1, 1)
+    simplex_table = np.vstack((a, -1 * c))
+    simplex_table = np.hstack((simplex_table, np.eye(simplex_table.shape[0]), np.vstack((b, np.zeros((1, 1))))))
+
+    min_indices = [None] * b.shape[0]
+    while(np.amin(simplex_table[-1]) < 0):
+        pivot_column = np.argmin(simplex_table[-1])
+        pivot_row = np.argmin(simplex_table[:-1, -1] / simplex_table[:-1, pivot_column])
+        min_indices[pivot_row] = pivot_column
+        simplex_table[pivot_row] /= simplex_table[pivot_row, pivot_column]
+        simplex_table[:pivot_row] -= simplex_table[:pivot_row, pivot_column].reshape(-1, 1) * simplex_table[pivot_row].reshape(1, -1)
+        simplex_table[pivot_row + 1:] -= simplex_table[pivot_row + 1:, pivot_column].reshape(-1, 1) * simplex_table[pivot_row].reshape(1, -1)
+
+    result = np.zeros_like(c)
+    for i in range(len(min_indices)):
+        if min_indices[i] is not None:
+            result[min_indices[i]] = simplex_table[i, -1]
+
+    return result
