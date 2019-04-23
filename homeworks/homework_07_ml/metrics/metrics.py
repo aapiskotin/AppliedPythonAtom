309e071f57ad04b6a78b51715e990fd46781309c
aapiskotin <aapiskotin@gmail.com>
2019-04-23 04:34:41 +0300

    metrics and titanic testready

--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -12,7 +12,10 @@ def logloss(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    eps = 1e-10
+    y_pred = np.clip(y_pred, eps, 1 - eps)
+
+    return - 1 / len(y_true) * np.sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
 
 
 def accuracy(y_true, y_pred):
@@ -22,7 +25,7 @@ def accuracy(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    return np.sum(np.equal(y_true, y_pred)) / len(y_pred)
 
 
 def presicion(y_true, y_pred):
@@ -32,7 +35,7 @@ def presicion(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    return np.sum(y_pred * y_true) / np.sum(y_pred)
 
 
 def recall(y_true, y_pred):
@@ -42,7 +45,7 @@ def recall(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    return np.sum(y_pred * y_true) / np.sum(y_true)
 
 
 def roc_auc(y_true, y_pred):
@@ -52,4 +55,17 @@ def roc_auc(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    n1 = np.sum(y_true)
+    n0 = len(y_true) - n1
+
+    auc = 0
+    fpr = 0
+    tpr = 0
+    for i in np.argsort(-y_pred):
+        if y_true[i] == 0:
+            auc += tpr / n0
+            fpr += 1 / n0
+        else:
+            tpr += 1 / n1
+
+    return auc
